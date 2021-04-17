75f58493391eca1d7a82137329d811c9711763fe
Roberto Pettinau <roberto.pettinau99@gmail.com>
2021-04-17 04:00:25 +0200

    Add ECB postfix attack

--- src/crypy/aes/aes.py
+++ src/crypy/aes/aes.py
@@ -1,4 +1,30 @@
+import math
 from Crypto.Util.strxor import strxor
+
+
+def postfix_attack(oracle, prefix_len=0):
+    found = b"A"*15
+    n_blocks = len(oracle(b""))//16
+
+    offset = math.ceil(prefix_len/16)
+
+    for b in range(n_blocks-offset):
+        for i in range(16):
+            for j in range(256):
+                new_byte = bytes([j])
+                block0 = b"P"*(16 - (prefix_len % 16)) if prefix_len % 16 != 0 else b""
+                block1 = found[-15:] + new_byte
+                block2 = b"A"*(15-i)
+                ct = oracle(block0+block1+block2)
+                ct_blocks = [ct[i:i+16] for i in range(0, len(ct), 16)]
+                if ct_blocks[offset+0] == ct_blocks[offset+b+1]:
+                    found += new_byte
+                    break
+
+    if prefix_len % 16 == 0:
+        return found[15:-1]
+    else:
+        return found[15:]
 
 
 def padding_oracle_attack(oracle, ct):
--- tests/test_aes.py
+++ tests/test_aes.py
@@ -1,24 +1,49 @@
-from crypy.aes import padding_oracle_attack
+from crypy.aes import padding_oracle_attack, postfix_attack
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
 
-def oracle_example(ct, gen_ct=None):
-    key = b"YELLOW SUBMARINE"
-    iv = key
-    aes = AES.new(key, AES.MODE_CBC, iv)
+def test_prefix_postfix_oracle():
+    secret = b"very-long-prefix-damn-this-is-long"
+    postfix = b"very-secret-formula-dont-share!!"
 
-    if gen_ct is not None:
-        return aes.encrypt(pad(gen_ct, 16))
+    for i in range(1, len(secret)+1):
+        prefix = secret[:i]
 
-    try:
-        unpad(aes.decrypt(ct), 16)
-        return True
-    except ValueError:
-        return False
+        def prefix_postfix_oracle(pt):
+            key = b"YELLOW SUBMARINE"
+            aes = AES.new(key, AES.MODE_ECB)
+            return aes.encrypt(pad(prefix+pt+postfix, 16))
+
+        assert postfix == postfix_attack(prefix_postfix_oracle, prefix_len=i)
+
+
+def test_postfix_oracle():
+    secret = b"very-secret-formula-dont-share!!"
+
+    for i in range(len(secret)+1):
+        postfix = secret[:i]
+
+        def postfix_oracle(pt):
+            key = b"YELLOW SUBMARINE"
+            aes = AES.new(key, AES.MODE_ECB)
+            return aes.encrypt(pad(pt+postfix, 16))
+
+        assert postfix == postfix_attack(postfix_oracle)
 
 
 def test_padding_oracle():
+    key = b"YELLOW SUBMARINE"
+    aes_enc = AES.new(key, AES.MODE_CBC, key)
+    aes_dec = AES.new(key, AES.MODE_CBC, key)
     pt = b"In cryptography, a padding oracle attack is an attack which uses the padding validation of a cryptographic message to decrypt the ciphertext."
-    ct = oracle_example("", gen_ct=pt)
-    assert pad(pt, 16)[16:] == padding_oracle_attack(oracle_example, ct)
+    ct = aes_enc.encrypt(pad(pt, 16))
+
+    def padding_oracle(ct):
+        try:
+            unpad(aes_dec.decrypt(ct), 16)
+            return True
+        except ValueError:
+            return False
+
+    assert pad(pt, 16)[16:] == padding_oracle_attack(padding_oracle, ct)
